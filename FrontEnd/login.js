68f9ef613433deff6e7c82d770fdcab82cf994b0
jaouhar ouertani <jaouhar.ouertani@gmail.com>
2023-05-18 11:39:48 +0200

    Version + dynamique de récupération de données et page login finie

---
+++ FrontEnd/login.js
@@ -0,0 +1,14 @@
+const login = () => {
+    // Récupération des informations d'identification
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+  
+    // Vérification des informations d'identification
+    if (email === "sophie.bluel@test.tld" && password === "S0phie") {
+      // Redirection vers la page d'admin
+      window.location.href = "http://127.0.0.1:5500/FrontEnd/index.html";
+    } else {
+      // Affichage d'une alerte "user not found"
+      alert("user not found");
+    }
+  };
--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,68 +1,102 @@
-//récupère la gallerie
-const gallery = document.querySelector(".gallery");
-
-//crée un élément "figure" dans la gallerie
-function createElement(element){
-  gallery.innerHTML +=`<figure class="imageFilter filtre${element.categoryId}"> <img crossorigin="anonymous" src="${element.imageUrl}" alt="${element.title}"> <figcaption>${element.title}</figcaption> <figure>`
-}
-
-//récupère les boutons
-const buttons = document.querySelectorAll("button");
-
-//permet d'ajouter la classe active au premier bouton par défaut
-buttons[0].classList.add("active");
-
-//boucle pour parcourir tout les boutons et ajoute la classe active au bouton cliqué tout en retirant cette classe aux autres boutons
-buttons.forEach(button => {
-  button.addEventListener("click", function() {
-    buttons.forEach(element => element.classList.remove("active"));
-    this.classList.add("active");
-  });
-});
-
-//fonction qui va filtrer les photos par leur id
-function filtre(filtreId){
-
-  //récupère les photos
-  const imageFilter = document.getElementsByClassName("imageFilter");
-
-  //boucle pour vérifier les photos
-  for(i = 0; i < imageFilter.length; i++){
-
-    //si l'id correspond, la classe "filtered" est enlevé
-    if (imageFilter[i].classList.contains(filtreId)){
-      imageFilter[i].classList.remove("filtered");
-    }
-
-    //si le bouton "tous" est selectionné, affiche toutes les images
-    else if (filtreId == "filtre0"){
-      imageFilter[i].classList.remove("filtered");
-    }
-
-    //applique la classe "filtered" si l'id ne correspond pas
-    else {
-      imageFilter[i].classList.add("filtered");
-    }
-  }
-}
-
+//fonction qui crée un élément "figure" dans la gallerie
+const createElement = (element) => {
+  const gallery = document.querySelector(".gallery");
+  gallery.innerHTML += `
+      <figure data-tag="${element.category.name}">
+          <img crossorigin="anonymous" src="${element.imageUrl}" alt="${element.title}">
+          <figcaption>${element.title}</figcaption>
+      </figure>`;
+};
 fetch("http://localhost:5678/api/works")
-
   //si fetch fonctionne on récupère les données au format JSON
-  .then(function(res) {
+  .then((res) => {
+    if (res.ok) {
+      return res.json();
+    }
+  })
+  
+  //récupération de chaque élément et effectue la fonction "createElement" pour chaque élément récupéré
+  .then((products) => {
+    products.forEach((product) => {
+      createElement(product);
+    });
+  })
+  //s'il y a une erreur, va console logger err
+  .catch(function (err) {
+    console.log(err);
+  });
+
+//fonction qui crée un bouton dans la nav #filtres
+const createButton = (button) => {
+  //on récupère le parent des boutons
+  const filtres = document.querySelector("#filtres");
+  //on ajoute le bouton
+  filtres.insertAdjacentHTML(
+    "beforeend",
+    `<button data-tag="${button.name}"> ${button.name} </button>`
+  );
+
+  //on récupère les boutons
+  const buttons = document.querySelectorAll("#filtres button");
+  //pour chaque bouton, si on clique dessus, fais une fonction
+  buttons.forEach((button) => {
+    button.addEventListener("click", function () {
+      //on enlève le classe "active" à tout les boutons
+      buttons.forEach((element) => element.classList.remove("active"));
+      //on met la classe "active" au bouton cliqué
+      this.classList.add("active");
+
+      //on récupère le tag du bouton
+      const buttonTag = this.dataset.tag;
+      //on récupère les figure faites plus haut
+      const images = document.querySelectorAll(".gallery figure");
+      //pour chaque figure on fait une fonction
+      images.forEach((image) => {
+        //si l'image a le même tag que le bouton, on enlève la classe "filtered"
+        if (image.getAttribute("data-tag") === buttonTag) {
+          image.classList.remove("filtered");
+          //on applique la classe "filtered" aux autres qui n'ont pas le même tag
+        } else {
+          image.classList.add("filtered");
+        }
+      });
+    });
+  });
+
+  //fonction qui va afficher toutes les images
+  function afficherToutesLesImages() {
+    // récupère toutes les images
+    const toutesLesImages = document.querySelectorAll(".gallery figure");
+    //on enlève la classe "filtered" a chaque images
+    toutesLesImages.forEach((image) => {
+      image.classList.remove("filtered");
+    });
+  }
+
+  //quand on clique sur "Tous", va faire la fonction afficherToutesLesImages
+  boutonTous.addEventListener("click", afficherToutesLesImages);
+};
+
+fetch("http://localhost:5678/api/categories")
+  //si fetch fonctionne on récupère les données au format JSON
+  .then((res) => {
     if (res.ok) {
       return res.json();
     }
   })
 
-  //récupération de chaque élément et effectue la fonction "createElement" pour chaque élément récupéré
-  .then(function(element) {
-    for(i = 0; i < element.length; i++){
-      createElement(element[i])
-    }
+  //récupération de chaque data et effectue la fonction "createButton" pour chaque data récupéré
+  .then((data) => {
+    data.forEach((button) => {
+      createButton(button);
+    });
   })
 
   //s'il y a une erreur, va console logger err
-  .catch(function(err) {
+  .catch(function (err) {
     console.log(err);
   });
+
+//permet d'ajouter la classe active au bouton "Tous" par défaut
+const boutonTous = document.querySelector('button[data-tag="Tous"]');
+boutonTous.classList.add("active");
