ee66ddf7bf35615c528a85b1a8d658bef87d09c3
jaouhar ouertani <jaouhar.ouertani@gmail.com>
2023-06-03 00:29:52 +0200

    "version finale"

--- FrontEnd/login/login.js
+++ FrontEnd/login/login.js
@@ -1,28 +1,30 @@
-const email = document.getElementById("email");
-const password = document.getElementById("password");
-const form = document.querySelector("form");
+const email = document.getElementById("email"); //récupère le champ de l'email
+const password = document.getElementById("password"); //récupère le champ du mot de passe
+const form = document.querySelector("form"); //récupère le formulaire
 
-email.setAttribute("autocomplete", "email");
-password.setAttribute("autocomplete", "current-password");
-
+//ajoute un addEventListener au formulaire. Lorsque le formulaire est soumis, la fonction d'après est executée
 form.addEventListener("submit", (event) => {
-    event.preventDefault();
+    event.preventDefault(); //empêche la soumission par défaut du formulaire, ce qui permet de contrôler ce qui se passe lorsque le formulaire est soumis.
 
+    //crée un objet user avec les valeurs d'email et de mot de passe entrées dans le formulaire.
     const user = {
         email: email.value,
         password: password.value,
     };
 
     fetch("http://localhost:5678/api/users/login", {
+        //la méthode est de type POST
         method: "POST",
         headers: {
             "Content-Type": "application/json;charset=utf-8",
         },
-        body: JSON.stringify(user),
+        body: JSON.stringify(user), //ajoute user en format JSON
     })
         .then((response) => {
+            //si la réponse à réussi on on récupère le contenu au format JSON
             if (response.status == 200) {
                 return response.json();
+                //sinon, affiche un message d'erreur
             } else if (!email.value || !password.value) {
                 let error = document.querySelector("p.error");
                 if (error) {
@@ -33,20 +35,25 @@ form.addEventListener("submit", (event) => {
                     `<p class="error">*Veuillez remplir tous les champs</p>`
                 );
             } else {
+                //supprimer le message d'erreur précédemment ajouté
                 let error = document.querySelector("p.error");
                 if (error) {
                     error.parentNode.removeChild(error);
                 }
+                //ajouter le message d'erreur
                 password.insertAdjacentHTML(
                     "afterend",
                     `<p class="error">*Erreur dans l'identifiant ou le mot de passe</p>`
                 );
             }
         })
+
         .then((data) => {
-            sessionStorage.setItem("token", data.token);
-            window.location.href = "/FrontEnd/index.html"; // Redirige vers la page index.html
+            sessionStorage.setItem("token", data.token); //stock le token dans un session storage.
+            document.location.href = "/FrontEnd/index.html"; //redirige vers la page index.html
         })
+
+        //s'il y a une erreur, va console logger err
         .catch((err) => {
             console.log(err);
         });
